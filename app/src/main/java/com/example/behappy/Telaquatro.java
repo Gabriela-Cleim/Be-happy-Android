d9dd1933235a820bb76f89f781ee532ce40f549e
Gabriela-Cleim <gabriela.cleims@gmail.com>
2022-02-04 13:21:15 -0300

    01

--- app/src/main/java/com/example/behappy/Telaquatro.java
+++ app/src/main/java/com/example/behappy/Telaquatro.java
@@ -2,13 +2,28 @@ package com.example.behappy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Telaquatro extends AppCompatActivity {
+
+    Button bt_prox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_telaquatro);
+
+        bt_prox=findViewById(R.id.bt_prox);
+
+        bt_prox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Telacinco=new Intent(view.getContext(), Telacinco.class);
+                startActivity(Telacinco);
+            }
+        });
     }
 }
