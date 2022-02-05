736dc7a1dd6a24cc5f31d9abd649e931a7b79a64
Gabriela-Cleim <gabriela.cleims@gmail.com>
2022-02-05 18:42:03 -0300

    chatbot
    antes de criar o chatbot

--- app/src/main/java/com/example/behappy/MainActivity.java
+++ app/src/main/java/com/example/behappy/MainActivity.java
@@ -2,33 +2,48 @@ package com.example.behappy;
 
 import static java.util.Objects.*;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     Button bt_entrar;
+    Button bt_help;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bt_entrar=findViewById(R.id.bt_entrar);
+        bt_entrar = findViewById(R.id.bt_entrar);
+        bt_help = findViewById(R.id.bt_help);
 
         bt_entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent Teladois=new Intent(view.getContext(), Teladois.class);
+                Intent Teladois = new Intent(view.getContext(), Teladois.class);
                 startActivity(Teladois);
             }
         });
 
+        bt_help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Você pode encontrar ajuda no chatbot sobre qualquer assunto!",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
+
 }
+
