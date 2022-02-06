d34a85acbaf1fbaa6bd19844d6f70d2183655832
Gabriela-Cleim <gabriela.cleims@gmail.com>
2022-02-05 23:07:08 -0300

    retro

--- app/src/main/java/com/example/behappy/chatbot.java
+++ app/src/main/java/com/example/behappy/chatbot.java
@@ -1,14 +1,66 @@
 package com.example.behappy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class chatbot extends AppCompatActivity {
+
+    private RecyclerView chatsRV;
+    private EditText userMsgEdt;
+    private FloatingActionButton sendMsgFAB;
+    private final String BOT_KEY = "bot";
+    private final String USER_KEY = "user";
+    private ArrayList<ChatsModal>chatsModalArrayList;
+    private ChatRVAdapter chatRVAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatbot);
+        chatsRV = findViewById(R.id.idRVChats);
+        userMsgEdt = findViewById(R.id.idEdtMessage);
+        sendMsgFAB = findViewById(R.id.idFABSend);
+        chatsModalArrayList = new ArrayList<>();
+        chatRVAdapter = new ChatRVAdapter(chatsModalArrayList,this);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        chatsRV.setLayoutManager(manager);
+        chatsRV.setAdapter(chatRVAdapter);
+
+
+        sendMsgFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(userMsgEdt.getText().toString().isEmpty()){
+                    Toast.makeText(chatbot.this, "Escreva uma mensagem!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                getResponse(userMsgEdt.getText().toString());
+                userMsgEdt.setText("");
+            }
+        });
+    }
+    private void getResponse(String message){
+        chatsModalArrayList.add(new ChatsModal(message, USER_KEY));
+        chatRVAdapter.notifyDataSetChanged();
+        String url = "http://api.brainshop.ai/get?bid=163527&key=OusQeXY1bRDsOb6D&uid=[uid]&msg="+message;
+        String BASE_URL = "http://api.brainshop.ai/";
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
     }
 }
